6130abe3152c8e9a0fc4fd2a590275278f42bad7
ShalishaManila <shalisha.manila@gmail.com>
2025-03-22 16:52:13 +0100

    completed javascript assignment

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-let price=prompt("Enter price tag");
+let usrPrice = prompt("Enter your price: ex: $40 ");
 
-console.log(`User Given price: ${price}`);
+console.log("User Given price: " , usrPrice);
+let usrPriceNumber = parseFloat(usrPrice.slice(1));
+
+console.log(usrPriceNumber);
+
+let discount = 0.1; //10%
+
+let discountedPrice = usrPriceNumber - (usrPriceNumber * discount);
+
+console.log("Y0ur New Price: $", discountedPrice);
